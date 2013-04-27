42b49bd0a55caebe13c6c6fbe2f86e169342eef7
Andrew Dunkman <andrew@dunkman.org>
2013-04-27 12:41:28 -0500

    Prevented anything but xhr-polling, as described by heroku: https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var server = http.createServer();
 var io = require("socket.io").listen(server);
 var port = process.env.PORT || 3000;
 
+io.configure(function () {
+  io.set("transports", [ "xhr-polling" ]);
+  io.set("polling duration", 10);
+})
+
 server.listen(port, function () {
   console.log("server listening on " + port);
 });
