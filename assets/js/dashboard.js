9cf3dd371573857b60107369fe41e868a0a8669a
Dusty Burwell <ascalonx@gmail.com>
2013-05-02 09:21:34 -0500

    Whops, commented localhost

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -9,8 +9,8 @@ var updateUI = function (users) {
 };
 
 $(document).ready(function () {
-  //var socket = io.connect('http://nodelabs.herokuapp.com:80');
-  var socket = io.connect('http://localhost:3000');
+  var socket = io.connect('http://nodelabs.herokuapp.com:80');
+  //var socket = io.connect('http://localhost:3000');
 
   socket.on("connect", function () {
     socket.emit("dashboard");
